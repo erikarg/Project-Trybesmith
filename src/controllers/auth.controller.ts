ef0afe75b9f4ea49fcfd1263f813fc906cadc729
erikarg <erikatrue@gmail.com>
2023-02-09 16:48:05 -0300

    fix: challenge 5

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import statusCode from '../helpers/statusCode';
 import AuthService from '../services/auth.service';
+import ErrorMessage from '../helpers/errorMessage';
 
 export default class AuthController {
   constructor(private authService = new AuthService()) {}
@@ -12,6 +13,11 @@ export default class AuthController {
   ): Promise<void> => {
     try {
       const result = await this.authService.validateData(req.body);
+      if (result === undefined) {
+        res
+          .status(statusCode.Unauthorized)
+          .json({ message: ErrorMessage.invalidUsername });
+      }
       const token = await this.authService.createToken(result);
       res.status(statusCode.OK).json({ token });
     } catch (err) {
